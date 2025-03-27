943bc88cc262dff12343f171c351997ee73d1c78
Jefferson Manzanares <fxymanzar3@gmail.com>
2025-03-27 12:17:21 -0600

    Final format corrections on graphics

--- Ejercicio_2/Ej_2.py
+++ Ejercicio_2/Ej_2.py
@@ -4,43 +4,63 @@ import os
 
 #Funcion de estudio
 def func(position, a = 3):
+    #Calcula el radio para todas las coordenadas de la malla
     r = np.sqrt(position[0]**2 + position[1]**2)
+
+    #Retorna una lista de valores, segun el valor de r (r>a da 0, r<=a da 1)
     return np.where(r <= a, 1, 0)
 
-#Grafica la funcion de estudio
-def plotFunc(pos,fValues, title = ""):
-    plt.pcolormesh(pos[0], pos[1], fValues, cmap='gray', shading='auto')
+#Grafica la funcion dada
+def plotFunc(pos, fValues, title, log = False):
+    #Grafica los valores dados
+    plt.pcolormesh(pos[0], pos[1], fValues, cmap='seismic')
+
+    #Indica en la barra si usa una escala logaritmica
+    if log == True:
+        plt.colorbar(label="Magnitud (Escala logaritmica)")
+    else:
+        plt.colorbar(label="Magnitud")
+
+    #Agrega el titulo y los limites de la grafica
     plt.title(title)
     plt.xlim(-5,5)
     plt.ylim(-5,5)
 
+#Define la dimension de la matriz cuadrada
+maxPoints = 500 
 
-# Crea la malla de coordenadas
-maxPoints = 1000
+#Crea una malla de coordenadas
 position = np.meshgrid(np.linspace(-5, 5, maxPoints), np.linspace(-5, 5, maxPoints))
 
-# Calcula los valores de la funcion en el plano (X,Y)
+#Calcula los valores de la funcion en la matriz de coordenadas
 funcValues = func(position)
 
 #Calcula la transformada de fourier de la funcion
 fourierValues = np.fft.fft2(funcValues)
 
-#Centra los valores de la frencuencia cero
+#Centra los valores de la frencuencia cero 
 fourierValues = np.fft.fftshift(fourierValues)
 
-#Calcula la transformada por su complemento
+#Calcula la transformada por su conjugado
 magFourier =np.abs(fourierValues) ** 2
 
-#Convierte las magnitudes a escala logaritmica para mejor visualizacion
+#Convierte las magnitudes a escala logaritmica para mejor visualizacion del cambio de valores de la transformada
 logMagFourier = np.log(magFourier + 1)
 
-#Grafica la funcion original y la transformada
-plt.figure(figsize=(8,4))
+#Define el tamano del plano
+plt.figure(figsize=(10,4))
+
+#Grafica la funcion de estudio
 plt.subplot(1,2,1)
 plotFunc(position, funcValues, title = "Funcion f(r)")
+
+#Grafica la transformada de la funcion por su complemento
 plt.subplot(1,2,2)
-plotFunc(position, logMagFourier, title = "Funcion F(k)F¯(k)")
+plotFunc(position, logMagFourier, title = "Funcion F(k)F¯(k)", log = True)
+
+#Se asegura que no se superponer ambas graficas
 plt.tight_layout()
-mainPath = os.path.dirname(__file__)
-plt.savefig(os.path.join(mainPath,"Grafica_Ejercicio2.png"))
+
+#Guarda la grafica y la muestra
+plt.savefig(os.path.join(os.path.dirname(__file__),"Grafica_Ejercicio2.pdf"))
 plt.show()
--- Ejercicio_5/Ej_5.py
+++ Ejercicio_5/Ej_5.py
@@ -25,56 +25,111 @@ def plotFourier(dataF, signalName,graphicsPath):
     imag = dataF[1]
 
     #Grafica la parte real de la transformada
-    plt.figure(figsize=(10,5))
+    plt.figure(figsize=(10,4))
     plt.subplot(1, 2, 1)
-    plt.pcolormesh(real, cmap='gray', shading='auto')
+    plt.pcolormesh(real,cmap='seismic')
     plt.title(f"Parte real de la señal {signalName}")
-    plt.ylabel("Magnitud")
-    plt.xlabel("Frecuencia (Hz)")
+    plt.ylabel("Y")
+    plt.xlabel("X")
+    plt.colorbar(label="Magnitud")
 
     # Grafica la parte imaginaria de la transformada
     plt.subplot(1, 2, 2)
-    plt.pcolormesh(imag, cmap='gray', shading='auto')
-    plt.ylabel("Magnitud")
+    plt.pcolormesh(imag,cmap='seismic')
+    plt.ylabel("Y")
     plt.title(f"Parte imaginaria de la señal {signalName}")
-    plt.xlabel("Frecuencia (Hz)")
+    plt.xlabel("X")
+    plt.colorbar(label="Magnitud")
     
     #Se asegura que no se superpongan las graficas
     plt.tight_layout()
 
     #Guarda la grafica
-    plt.savefig(os.path.join(graphicsPath,f"Transformada_de_{signalName}.pdf"))
+    plt.savefig(os.path.join(graphicsPath,f"Transformada del {signalName}.pdf"))
+    plt.show()
+
+def plotArray(arrayList,nameList, graphicsPath, title):
+    plt.figure(figsize=(5*len(arrayList),4))
+    for i in range(0,len(arrayList)):
+        
+        plt.subplot(1, len(arrayList), i+1)
+        plt.pcolormesh(arrayList[i], cmap='grey')
+        plt.title(f"Grafica {nameList[i]}")
+        plt.colorbar(label="Magnitud")
+        plt.ylabel("Y")
+        plt.xlabel("X")
+
+    #Se asegura que no se superpongan las graficas
+    plt.tight_layout()
+
+    #Guarda la grafica
+    plt.savefig(os.path.join(graphicsPath,f"{title}.pdf"))
     plt.show()
 
 #Array solicitados
-data1 = np.zeros((30, 30))
-data2 = np.zeros((30, 30))
-data3 = np.zeros((30, 30))
+array1 = np.zeros((30, 30))
+array2 = np.zeros((30, 30))
+array3 = np.zeros((30, 30))
 
 #Cambia los puntos solicitados de los arrays a 1
-data1[5, 5] = 1
-data1[5, 10] = 1
-data2[5, 5] = 1
-data2[25, 5] = 1
-data3[10, 15] = 1
-data3[25, 20] = 1
+array1[5, 5] = 1
+array1[5, 10] = 1
+array2[5, 5] = 1
+array2[25, 5] = 1
+array3[10, 15] = 1
+array3[25, 20] = 1
 
-#Crea una lista con los 3 arrays
-dataList = [data1, data2, data3]
-
+#Define la carpeta donde se van a guardar las graficas
 graphicsPath = os.path.join(os.path.dirname(__file__),"Graphics")
 
+#Crea una lista con los 3 arrays
+arrayList = [array1, array2, array3]
 
 #Por cada array en la lista
 contador = 1
-for data in dataList:
+for array in arrayList:
 
     #Nombre del array
-    nombre = f"Array_{contador}"
+    nombre = f"Array {contador}"
     contador += 1
 
     #Calcula la transformada de fourier del array
-    dataFourier = fourierTransformation(data)
+    dataFourier = fourierTransformation(array)
 
     #Genera el grafico de la transformada del array
     plotFourier(dataFourier,nombre,graphicsPath)
+
+#Rota el primer array1 90 grados en sentido horario
+array1Rotado = np.rot90(array1)
+plotArray([array1,array1Rotado],["Array1", "Array1 Rotado"],graphicsPath, "Comparacion Rotacion Array 1")
+
+#Acerca un punto del array2 
+array2Acercado = np.zeros((30, 30))
+array2Acercado[25,5] = 1
+array2Acercado[15,5] = 1
+plotArray([array2,array2Acercado],["Array2", "Array2 Acercado"],graphicsPath, "Comparacion Acercamiento Array 2")
+
+#Aleja un punto del array2 
+array2Alejado = np.zeros((30, 30))
+array2Alejado[25,5] = 1
+array2Alejado[0,5] = 1
+plotArray([array2,array2Alejado],["Array2", "Array2 Alejado"],graphicsPath, "Comparacion Alejamiento Array 2")
+
+#Mueve un punto de array 3 en 45 grados
+array3Movido = np.zeros((30, 30))
+array3Movido[10, 15] = 1
+array3Movido[5, 0] = 1 
+plotArray([array3,array3Movido],["Array3", "Array3 Movido"],graphicsPath, "Comparacion Movimiento en angulo Array 3")
+
+#Crea una lista con los 4 arrays
+arrayNameList = ["Array1 Rotado", "Array2 Acercado", "Array2 Alejado", "Array3 Movimiento en Angulo"]
+arrayList = [array1Rotado, array2Acercado, array2Alejado, array3Movido]
+
+#Por cada array en la lista
+for i in range(0,len(arrayList)):
+
+    #Calcula la transformada de fourier del array
+    dataFourier = fourierTransformation(arrayList[i])
+
+    #Genera el grafico de la transformada del array
+    plotFourier(dataFourier,arrayNameList[i],graphicsPath)
